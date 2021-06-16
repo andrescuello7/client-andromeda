6b3a73119ad69ebbc4b52979d7fab0d4e6ae7ef0
andrescuello7 <andrescuellotrabajo@gmail.com>
2021-06-16 17:05:09 -0300

    actualice login y estetica de register

--- src/pages/PerfilBuscado/PerfilBuscado.js
+++ src/pages/PerfilBuscado/PerfilBuscado.js
@@ -1,22 +1,28 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { Card, NavDropdown, Spinner } from "react-bootstrap";
+import { useState } from "react";
 import UseBusqueda from "../../UseForm/UseBusqueda";
-import UseHome from "../../UseForm/UseHome";
 import PortadaBuscada from "../../components/PortadaBuscada/PortadaBuscada";
+import Barra from "../../components/Barra/Barra";
 
 const Perfil = () => {
-  const [input, setInput] = useState({});
-  const token = localStorage.getItem("token");
   const { MapDataBaseBuscado } = UseBusqueda();
 
   return (
-    <div className="ColorDePerfil">
-      <div>
-        <PortadaBuscada />
+    <div>
+      <div className="bodyLandPage">
+        <div className="barra sticky-top">
+          <Barra />
+        </div>
+        <div className="ColorDePerfil perfil">
+          <div>
+            <PortadaBuscada />
+          </div>
+          <div className="w-100 d-flex flex-column-reverse mt-5">
+            {MapDataBaseBuscado}
+          </div>
+          <div className="mt-5"></div>
+        </div>
+        <div className="perfilAnuncios"></div>
       </div>
-      <div className="w-100 d-flex flex-column-reverse mt-5">{MapDataBaseBuscado}</div>
-      <div className="mt-5"></div>
     </div>
   );
 };
